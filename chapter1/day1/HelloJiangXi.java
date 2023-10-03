ea46ff497b244f485efe75353e9e378547d714ff
2391941433@qq.com <2391941433@qq.com>
2023-10-04 00:49:06 +0800

    学习通过cmd编译和运行java程序

---
+++ chapter1/day1/HelloJiangXi.java
@@ -0,0 +1,17 @@
+// 一个源文件里可以写多个 class
+// 一个源文件里只可以有一个 class 用 public 修饰，且这个被修饰的 class 必须和文件名同名
+
+public class HelloJiangXi {
+    public static void main(String[] args) {
+        System.out.println("HelloJiangXi");
+    }
+}
+
+class HelloNanChang {
+
+}
+
+class HelloJiuJiang {
+
+}
+
---
+++ chapter1/day1/HelloWorld.java
@@ -0,0 +1,7 @@
+// .java����Դ�ļ�   .class�����ֽ����ļ�   �ֽ����ļ��Ǹ������������ɵ�
+// ͨ������ javac �����������ֽ����ļ��� ͨ������ java �����������ֽ����ļ�
+class HelloChina {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!!��ã��й���");
+    }
+}
